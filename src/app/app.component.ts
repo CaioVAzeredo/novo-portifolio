bb7ffa524076df2a87e312fc73f84a95420b1213
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-03-26 23:02:06 -0300

    adição do html

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { HeroComponent } from "./componentes/hero/hero.component";
+import { TrabalhoComponent } from "./componentes/trabalho/trabalho.component";
+import { SobreComponent } from "./componentes/sobre/sobre.component";
+import { ContatoComponent } from "./componentes/contato/contato.component";
+import { FooterComponent } from "./componentes/footer/footer.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    CabecalhoComponent,
+    HeroComponent,
+    TrabalhoComponent,
+    SobreComponent,
+    ContatoComponent,
+    FooterComponent
+],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+}
---
+++ src/app/componentes/trabalho/trabalho.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trabalho',
+  imports: [],
+  templateUrl: './trabalho.component.html',
+  styleUrl: './trabalho.component.css'
+})
+export class TrabalhoComponent {
+
+}
