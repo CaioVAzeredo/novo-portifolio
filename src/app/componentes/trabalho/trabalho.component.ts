545b4ce903e387558444e407f731e0e02a820a93
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-04-15 23:57:05 -0300

    adição do for para iterar a lista de trabalhos

--- src/app/componentes/trabalho/trabalho.component.ts
+++ src/app/componentes/trabalho/trabalho.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-trabalho',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './trabalho.component.html',
-  styleUrl: './trabalho.component.css'
+  styleUrls: ['./trabalho.component.css']
 })
-export class TrabalhoComponent {
+export class TrabalhoComponent implements OnInit {
+  trabalhos: any[] = [];
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get<any>('/assets/trabalhos.json')
+      .subscribe(data => {
+        this.trabalhos = data.trabalhos;
+        console.log(this.trabalhos);
+      });
+  }
 }
